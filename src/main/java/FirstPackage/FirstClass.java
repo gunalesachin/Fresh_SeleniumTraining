f8e0016eb39f30081a30412eb0f0c37c25f6b520
gunalesachin <41350684+gunalesachin@users.noreply.github.com>
2023-08-09 07:29:37 +0000

    Pushing First Update

--- src/main/java/FirstPackage/FirstClass.java
+++ src/main/java/FirstPackage/FirstClass.java
@@ -1,5 +1,16 @@
 package FirstPackage;
 //TestClass
-public class FirstClass {
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass 
+{
+	public static void main(String[] args)
+	{
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.navigate().to("https://google.com/");
+	
+	}
 }
