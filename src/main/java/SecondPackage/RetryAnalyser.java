bd2871692b6831a5d6b2e465891fa9b5e9d0e2fa
Administrator <Administrator@172.31.2.105>
2023-08-21 07:23:51 +0000

    pushing iretrate

---
+++ src/main/java/SecondPackage/RetryAnalyser.java
@@ -0,0 +1,17 @@
+package SecondPackage;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.IretryAnalyzer;
+
+public class RetryAnalyser implements IRetryAnalyzer{
+
+	Private int 
+	public boolean retry(ITestResult result) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
+
+	
+
